911fd01732f43d031f784fd0e8e8d6032cc0cc56
shivasoni66 <shivasonisankal@example.com>
2026-05-12 18:27:02 +0530

    Fix: Added missing video_player dependency to pubspec.yaml

--- lib/features/reels/presentation/post_reel_screen.dart
+++ lib/features/reels/presentation/post_reel_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
 import '../../../core/theme/app_colors.dart';
 import '../../../core/theme/app_text_styles.dart';
 
